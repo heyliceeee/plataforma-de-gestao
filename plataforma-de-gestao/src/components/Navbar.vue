42e2b6109ce025729be5b472a71aba5d064137ef
Alice Dias <8200118@estg.ipp.pt>
2025-02-10 19:06:37 +0000

    ✨feat: save last searched city in localstorage

--- plataforma-de-gestao/src/components/Navbar.vue
+++ plataforma-de-gestao/src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-            <a class="navbar-brand" to="/">🌎 Weather Dashboard</a>
+            <a class="navbar-brand" to="/">🌎 Weather App</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
@@ -13,7 +13,7 @@
                         <router-link class="nav-link" to="/">🏠 Home</router-link>
                     </li>
                     <li class="nav-item">
-                        <router-link class="nav-link" to="/history">🕒 History</router-link>
+                        <router-link class="nav-link" to="/history">📜 History</router-link>
                     </li>
                     <li class="nav-item">
                         <router-link class="nav-link" to="/settings">⚙️ Settings</router-link>
