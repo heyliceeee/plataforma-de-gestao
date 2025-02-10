fa5b7e15ce014dc2cb7bb0d7bad61498c9e956eb
Alice Dias <8200118@estg.ipp.pt>
2025-02-10 13:37:17 +0000

    🎉initialize project

---
+++ plataforma-de-gestao/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+  <div class="container">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+      <a class="navbar-brand" href="#">Alice Dias</a>
+    </nav>
+    <router-view />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ plataforma-de-gestao/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="mt-4">
+
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            dpps: [
+                { id: 1, nome: "Produto A", status: "Ativo" },
+                { id: 2, nome: "Produto B", status: "Inativo" }
+            ]
+        };
+    }
+};
+</script>
